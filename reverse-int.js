4e14430965c4669957f026dedf451a3eaeb249b7
Tahsin Shahriar <tahsin.ewucse@gmail.com>
2023-02-23 13:27:42 +0600

    code refactored

--- reverse-int.js
+++ reverse-int.js
@@ -9,25 +9,27 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-    const arr = String(n).split("");
-    const modArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == "-") {
-            modArr.unshift(arr[i]);
-        } else if (arr[i] == "0") {
-            continue;
-        } else {
-            modArr.push(arr[i]);
-        }
-    }
-    if (modArr[0] != "-") {
-        return Number(modArr.reverse().join(""));
-    } else {
-        modArr.splice(0, 1);
-        modArr.reverse();
-        modArr.unshift("-");
-        return Number(modArr.join(""));
-    }
+    // const arr = String(n).split("");
+    // const modArr = [];
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] == "-") {
+    //         modArr.unshift(arr[i]);
+    //     } else if (arr[i] == "0") {
+    //         continue;
+    //     } else {
+    //         modArr.push(arr[i]);
+    //     }
+    // }
+    // if (modArr[0] != "-") {
+    //     return parseInt(modArr.reverse().join(""));
+    // } else {
+    //     modArr.splice(0, 1);
+    //     modArr.reverse();
+    //     modArr.unshift("-");
+    //     return parseInt(modArr.join(""));
+    // }
+    const reversed = n.toString().split("").reverse().join("");
+    return parseInt(reversed) * Math.sign(n);
 }
 
-console.log(reverseInt(-981));
+console.log(reverseInt(-900));
